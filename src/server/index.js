e799db3d300cc0a3b840c74ac4a8143a3808ea2f
DarkXipher <darx78@yahoo.com>
2023-08-22 18:33:20 -0500

    add path

--- src/server/index.js
+++ src/server/index.js
@@ -42,13 +42,13 @@ app.get(/^\/(player|work|circle|tag|va)s?\/(\d+)?$/, (req, res) => {
 app.use(express.static('dist'));
 
 // If 404'd, serve from 'static' folder
-app.use('/static', express.static('static'));
+app.use('/kikoeru/static', express.static('static'));
 
 // Expose API routes
-app.use('/api', routes);
+app.use('/kikoeru/api', routes);
 
 // Expose authentication route
-app.use('/auth', authRoutes);
+app.use('/kikoeru/auth', authRoutes);
 
 // Start server
 app.listen(process.env.PORT || 8888, () => {
