5152f64357acb8623876f3bdf00048a623a17fb4
DarkXipher <darx78@yahoo.com>
2023-08-22 19:08:32 -0500

    revert

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   entry: ['babel-polyfill', './src/client/index.jsx'],
   output: {
     path: path.join(__dirname, 'dist'),
-    publicPath: '/kikoeru/',
+    publicPath: '/',
     filename: 'bundle.js',
   },
   module: {
